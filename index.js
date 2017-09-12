08cdacce283341e71ff506ad81b264dabfddbab6
Mary Luong <marysluong@gmail.com>
2017-09-12 22:56:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,13 +21,9 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() === uppercase) {
-    return "YES INDEED!"
-  }
-}
-
-function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
+  } else if (string.toUpperCase() === uppercase) {
+    return "YES INDEED!"
   }
 }
